c542b966cef4feaf49ba783f7094ec9b32dda44f
Rinat Abubekerov <89776870800@yandex.ru>
2022-09-24 15:56:23 +0300

    Привет ЛариСа

---
+++ Example005_Condition_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лариса")
+{
+    System.Console.WriteLine("Ура, это же Лариса!!!!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
